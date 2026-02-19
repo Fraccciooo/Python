7c15611b9db3601cb88defe5125dc3a9b8a0f747
Fraccciooo <ignaciofrachia.programs@gmail.com>
2026-02-18 21:04:41 -0400

    Listo, ahora si, faltaron agregar comentarios en el archivo ManejoDeExcepciones.js

--- Python_Curso/Excepciones/ManejoDeExcepciones.py
+++ Python_Curso/Excepciones/ManejoDeExcepciones.py
@@ -1,4 +1,7 @@
-
+# En este apartado se trata de entender todo la relacionado con las entradas invalidas
+# se mejoran mediante la incorporacion de resultados optenidos por la consola
+# dichos resultados son errados, por lo tanto, con las exceptiones, se manejan con tranquilidad
+# esos errores (numericos, caracteres, espacioes vacios, etc...)
 
 try:
     numerador = int(input("Ingrese un numero: "))
